67936172d056f0446e6fa462b95d63868567df16
Flavio Mendes <fhenrique609@gmail.com>
2021-10-30 21:05:42 -0300

    E-Diaristas mobile

---
+++ src/data/services/ApiService.ts
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+const url = "https://rocky-sierra-65016.herokuapp.com/"
+
+export const ApiService = axios.create({
+    baseURL: url,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
